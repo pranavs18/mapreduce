7ea59616761e695a87edc2c62246b6b46796a102
Pranav Saxena <psbits@gmail.com>
2014-07-10 14:01:33 -0400

    Task tracker within the slave(worker) and job status

---
+++ src/generics/JobStatus.java
@@ -0,0 +1,14 @@
+/******************************************************************************************
+ * 
+ * @author - Pranav Saxena/ Vaibhav Suresh Kumar
+ * 
+ * JobStatus is an enum for check status of the mapers and reduces.
+ * 
+ ****************************************************************************************/
+
+package generics;
+
+public enum JobStatus {
+
+	RUNNING, COMPLETE, AVAILABLE;
+}
