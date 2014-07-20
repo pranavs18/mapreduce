3f9580a5d6b63d4222f13198840bd1e21c2d9356
Vaibhav Suresh Kumar <vaibhav.sureshkumar@gmail.com>
2014-07-20 04:50:13 -0400

    moving chunk transfer request to master-name node inteface

--- src/generics/MasterToNameNodeInterface.java
+++ src/generics/MasterToNameNodeInterface.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -13,5 +14,5 @@ public interface MasterToNameNodeInterface  extends Remote {
 
 
 	public ConcurrentHashMap<String, ChunkProperties> sendChunkMap(MapReduceConfiguration config, Set<String> workerIpAddresses , String splitIp) throws RemoteException, NotBoundException, FileNotFoundException, IOException;
-
+	public String requestForChunkTransfer(String chunkName, ArrayList<String> visitedIPs , Set<String> workerIps) throws FileNotFoundException, IOException, NotBoundException;
 }
