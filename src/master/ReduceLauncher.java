1fae3dd1153ad781d9946838f0d7f4bf895c5ff5
Vaibhav Suresh Kumar <vaibhav.sureshkumar@gmail.com>
2014-07-22 12:03:04 -0400

    master Reduce Launcher added

---
+++ src/master/ReduceLauncher.java
@@ -0,0 +1,44 @@
+package master;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+
+import generics.MapReduceConfiguration;
+import generics.MasterToWorkerInterface;
+
+public class ReduceLauncher implements Runnable{
+
+
+	MapReduceConfiguration config;
+	String ip;
+
+	public ReduceLauncher(MapReduceConfiguration config, String ip) {
+		this.config = config;
+		this.ip = ip;
+	}
+
+
+	public void launchReduce(){
+		MasterToWorkerInterface launchReduce = null;
+		try {
+			launchReduce = (MasterToWorkerInterface)Naming.lookup("rmi://"+ip+":9876/job");
+
+			Boolean isJobLaunchSuccessful = launchReduce.launchReduceJob(config);
+			
+			if(isJobLaunchSuccessful == true){
+				MasterGlobalInformation.getIncreasedReducerSuccessCount();
+			}
+
+		} catch (NotBoundException | IOException | ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException | SecurityException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+
+	}
+	@Override
+	public void run() {
+
+	}
+
+}
